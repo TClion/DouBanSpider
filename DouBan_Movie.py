564bfea214835ce46e2fe7d6a56854f82bf22a9a
TClion <763242665@qq.com>
2017-05-14 19:05:05 +0800

    2017/5/14

--- DouBan_Movie.py
+++ DouBan_Movie.py
@@ -22,19 +22,9 @@ moviehead = {
 class DouBan():
     def __init__(self):
         self.Url = "https://movie.douban.com/tag/"      #豆瓣电影分类
-<<<<<<< HEAD
         conn = pymongo.MongoClient('localhost',27017)     #连接mongodb
         self.db = conn.douban
         self.movie = self.db.movie
-=======
-        self.L = []                 #去重列表
-        self.N = 0                  #计数器
-        self.db = pymysql.connect('localhost','root','password','doubanmovie',charset='utf8')  #连接数据库
-        self.cursor = self.db.cursor()
-        self.db.autocommit(True)
-        manager = multiprocessing.Manager()     #创建队列
-        self.Queue = manager.Queue()
->>>>>>> origin/master
 
 
 
@@ -50,11 +40,7 @@ class DouBan():
 
     #抓取电影信息url
     def GetMovie(self,key,url):
-<<<<<<< HEAD
         for i in range(0,200,20):       #制作分页url
-=======
-        for i in range(0,20,20):       #制作分页url
->>>>>>> origin/master
             Url =url + '?start='+str(i)+'&type=T'
             Moviehtml = requests.get(Url,headers=moviehead).text
             movielist = etree.HTML(Moviehtml)
@@ -95,17 +81,6 @@ class DouBan():
 
 if __name__ == '__main__':
     movie = DouBan()
-<<<<<<< HEAD
     movielist = movie.GetList()
     for key,url in movielist:
         movie.GetMovie(key,url)
-=======
-    movie.CreateTable1()
-    movie.GetList()
-    pool = Pool(processes=1)
-    for key,url in movie.ClassList:
-        pool.apply_async(movie.GetMovie,(key,url,))
-    movie.GetInfo()
-    pool.close()
-    pool.join()
->>>>>>> origin/master
