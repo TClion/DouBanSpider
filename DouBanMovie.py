02e045fc246e89e00a4f7d4919db5fdb5d8e6809
TClion <763242665@qq.com>
2017-05-14 19:00:49 +0800

    2017/5/14

--- DouBanMovie.py
+++ DouBanMovie.py
@@ -1,11 +1,9 @@
-"""豆瓣电影爬虫，抓取豆瓣电影上的电影信息，需要填写自己的数据库信息"""
+"""豆瓣电影爬虫，抓取豆瓣电影上的电影信息，放入Mongodb中"""
 import requests
 from lxml import  etree
 import urllib.parse
-import pymysql
-from multiprocessing.dummy import Pool
 import time
-import multiprocessing
+import pymongo
 
 
 moviehead = {
@@ -24,100 +22,65 @@ moviehead = {
 class DouBan():
     def __init__(self):
         self.Url = "https://movie.douban.com/tag/"      #豆瓣电影分类
-        self.L = []                 #去重列表
-        self.N = 0                  #计数器
-        self.db = pymysql.connect('localhost','root','topcoder','doubanmovie',charset='utf8')  #连接数据库
-        self.cursor = self.db.cursor()
-        self.db.autocommit(True)
-        manager = multiprocessing.Manager()     #创建队列
-        self.Queue = manager.Queue()
+        conn = pymongo.MongoClient('localhost',27017)     #连接mongodb
+        self.db = conn.douban
+        self.movie = self.db.movie
 
 
 
     #获取电影所有分类
     def GetList(self):
-        bookhtml = requests.get(self.Url,headers=moviehead).content
+        bookhtml = requests.get(self.Url,headers=moviehead).text
         Item = etree.HTML(bookhtml)
-        List = Item.xpath('//tbody/tr/td/a/text()')
+        List = Item.xpath('//table[@class="tagCol"]/tbody/tr/td/a/text()')
         self.classlist = [self.Url+urllib.parse.quote(x) for x in List]
         self.ClassList = list(zip(List,self.classlist))
-        print(self.ClassList)
+        return self.ClassList
 
 
     #抓取电影信息url
     def GetMovie(self,key,url):
-        for i in range(0,40,20):       #制作分页url
+        for i in range(0,200,20):       #制作分页url
             Url =url + '?start='+str(i)+'&type=T'
             Moviehtml = requests.get(Url,headers=moviehead).text
             movielist = etree.HTML(Moviehtml)
             Movieurl = movielist.xpath('//div[@class="pl2"]/a/@href')
-            for movieurl in Movieurl:
-                if movieurl not in self.L:  #去重
-                    self.L.append(movieurl)
-                    self.Queue.put(movieurl)
-        if len(self.L)>5000:
-            self.L = []
-        self.Queue.put('stop')  #代表一个分类抓取完毕
-        time.sleep(10)
+            for u in Movieurl:
+                if self.movie.find_one({'url':u})==None:    #如果数据库中没有影片对应的url就抓取
+                    self.GetInfo(key,u)
+            time.sleep(2)
+        print('%s抓取完毕' % key)
 
 
-    def GetInfo(self):
-        while True:
-            if self.N == 136:   #豆瓣电影一共136个分类
-                break
-            if not self.Queue.empty():
-                movieurl = self.Queue.get()
-                if movieurl == 'stop':
-                    print(self.ClassList[self.N][0]+'抓取完毕')
-                    self.N+=1
-                    continue
-                MovieHtml = requests.get(movieurl,headers=moviehead).text
-                InfoPage = etree.HTML(MovieHtml)
-                try:
-                    moviename = InfoPage.xpath('//a[@data-mode="plain"]/@data-name')[0]
-                    basicinfo = InfoPage.xpath('//a[@data-mode="plain"]/@data-desc')[0]
-                    evaluate  = ' '.join(InfoPage.xpath('//div[@class="indent"]/span/text()'))
-                    hotreview = ' '.join(InfoPage.xpath('//div[@class="comment"]/p/text()'))
-                    Moviename = moviename.replace('\u200e ','')
-                    eva = evaluate.replace(' ','').replace('\n','').replace('\u3000\u3000','').strip()
-                    result = [key,Moviename,basicinfo,eva]
-                    time.sleep(2)       #设置间隔时间
-                    self.SaveMovie(result)
-                except Exception as e:
-                    print(e)
-                    self.db.rollback()
-
-    #创建储存信息的表
-    def CreateTable1(self):
+    def GetInfo(self,key,movieurl):
+        MovieHtml = requests.get(movieurl,headers=moviehead).text
+        InfoPage = etree.HTML(MovieHtml)
         try:
-            sql1 = """create table movie(
-                      classify char(200) not null,
-                      moviename char(200) not null,
-                      info text not null,
-                      eva text not null
-)"""
-            self.cursor.execute(sql1)
+            moviename = InfoPage.xpath('//a[@data-mode="plain"]/@data-name')[0]
+            basicinfo = InfoPage.xpath('//a[@data-mode="plain"]/@data-desc')[0]
+            evaluate  = ' '.join(InfoPage.xpath('//div[@class="indent"]/span/text()'))
+            #hotreview = ' '.join(InfoPage.xpath('//div[@class="comment"]/p/text()'))
+            Moviename = moviename.replace('\u200e ','')
+            #eva = evaluate.replace(' ','').replace('\n','').replace('\u3000\u3000','').strip()
+            result = [key,Moviename,movieurl,basicinfo]
+            time.sleep(2)       #设置间隔时间
+            self.SaveMovie(result)
         except Exception as e:
             print(e)
-            self.db.rollback()
 
-    #存放入库
+    #向mongodb中插入数据
     def SaveMovie(self,List):
-        try:
-            sqli = """insert into movie values (%s,%s,%s,%s)"""
-            self.cursor.execute(sqli,List)
-        except Exception as e:
-            print(e)
-            self.db.rollback()
+        D = {}
+        D['key'] = List[0]
+        D['moviename'] = List[1]
+        D['url'] = List[2]
+        D['info'] = List[3]
+        self.movie.insert(D)
+        print(D)
 
 
 if __name__ == '__main__':
     movie = DouBan()
-    movie.CreateTable1()
-    movie.GetList()
-    pool = Pool(processes=1)
-    for key,url in movie.ClassList:
-        pool.apply_async(movie.GetMovie,(key,url,))
-    movie.GetInfo()
-    pool.close()
-    pool.join()
+    movielist = movie.GetList()
+    for key,url in movielist:
+        movie.GetMovie(key,url)
