f4a684cdfef8da1db0e733d06947a9cf2baa13d6
TClion <763242665@qq.com>
2017-10-08 00:39:08 +0800

    Update DouBanRead.py

--- DouBanRead.py
+++ DouBanRead.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # coding=utf8
 
-# version:1.0
+# version:2.0
 # kali linux python 2.7.13
 # author:TClion
 # update:2017-10-07
 # 豆瓣读书信息抓取，redis去重，数据存入mongodb中
-
 
 import time
 import redis
@@ -28,12 +27,11 @@ class spider():
         self.conn = pymongo.MongoClient('localhost', 27017)
         self.R = redis.Redis(host='localhost', port=6379)
         self.db = self.conn['douban']
-        self.data_coll = self.db['book']
+        self.data_coll = self.db['booktest']
         self.ip_coll = self.conn['ipdb']['ip_good']
-        self.insert_success_num = 0     #抓取成功数量
+        self.redis_key = 'booktest'
         self.ip_lst = self.get_ip_lst_m()
         self.ip = random.choice(self.ip_lst)
-        self.pool = Pool(200)
 
     #从ip库中读取ip
     def get_ip_lst_m(self):
@@ -49,7 +47,7 @@ class spider():
             }
             ip_lst.append(ip_dict)
         return ip_lst
-    
+
     #目录链接
     def parse_item_lst(self):
         content = requests.get(self.Url).content
@@ -57,8 +55,8 @@ class spider():
         item_lst = data.xpath('//table[@class="tagCol"]/tbody/tr/td/a/text()')
         self.item_lst = ['https://book.douban.com/tag/' + urllib.quote(u.encode('utf-8')) for u in item_lst]
 
-    #抓取列表页url
-    def parse_info_url(self, url):
+    #使用代理ip打开网页,返回content
+    def open_url(self, url):
         while True:
             try:
                 page = requests.get(url, proxies=self.ip, timeout=5)
@@ -71,36 +69,28 @@ class spider():
                     print 'ip list empty'
                     self.ip_lst = self.get_ip_lst_m()
         content = page.text
+        return content
+
+    #抓取列表页url
+    def parse_info_url(self, url):
+        content = self.open_url(url)
         data = etree.HTML(content)
-        print '%s parse successful' % url
         url_lst = data.xpath('//div[@class="info"]/h2/a/@href')
-        self.pool.map(self.parse, url_lst)
+        for i in url_lst:
+            self.R.sadd(self.redis_key, i)
+        print 'list url %s parse successful' % url
         try:
             next_page = data.xpath('//span[@class="next"]/a/@href')[0]
-            f, s = next_page.split('?')
-            f = urllib.quote(f)
-            next_page = f + '?' + s
-            new_url = urljoin(url, next_page)
-            self.parse_info_url(new_url)
         except:
             return
+        new_url = urljoin(url, next_page)
+        self.parse_info_url(new_url)
 
     #抓取详情页信息
     def parse(self, url):
-        if url in self.R.smembers('book') and self.data_coll.find_one({'url': url}):
+        if self.data_coll.find_one({'url': url}) != None:
             return
-        while True:
-            try:
-                page = requests.get(url, proxies=self.ip, timeout=5)
-                break
-            except:
-                try:
-                    self.ip_lst.remove(self.ip)
-                    self.ip = random.choice(self.ip_lst)
-                except:
-                    print 'ip list empty'
-                    self.ip_lst = self.get_ip_lst_m()
-        content = page.text
+        content = self.open_url(url)
         if u'呃...你想访问的页面不存在' in content:
             return
         data = etree.HTML(content)
@@ -113,17 +103,17 @@ class spider():
             'title': title,
         }
         self.data_coll.insert(post)
-        self.insert_success_num += 1
-        self.R.sadd('book', url)
-        print '%s parse successful' % url
-        print self.insert_success_num
+        print 'info url %s parse successful' % url
 
 
 if __name__ == '__main__':
     douban = spider()
     douban.parse_item_lst()
-    thread = [gevent.spawn(douban.parse_info_url, i) for i in douban.item_lst]
-    gevent.joinall(thread)
+    gp = Pool(100)
+    gp.map(douban.parse_info_url, douban.item_lst)
+
+    # gp2 = Pool(200)
+    # gp2.map(douban.parse, douban.R.smembers(douban.redis_key))
 
 
 
